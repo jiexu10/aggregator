44127f1b3607e2561e9e5ecb30fd2d593f0e1510
jiexu10 <jie.xu.10@gmail.com>
2016-03-06 12:02:43 -0500

    added users through devise

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,7 @@ require 'capybara/rails'
 require 'valid_attribute'
 require 'factory_girl_rails'
 require 'pry'
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
